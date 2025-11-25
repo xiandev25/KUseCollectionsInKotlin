21327759193116301d7909d64a8e58881c8b5729
Xian Dev <xiandev25@gmail.com>
2025-11-25 20:56:22 +0100

    Access the elements of an array

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,15 @@
 fun main() {
     val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
     val gasPlanets = arrayOf("Jupiter", "Satan", "Uranus", "Neptune")
+
     val solarSystem = rockPlanets + gasPlanets
+
+    println(solarSystem[0])
+    println(solarSystem[1])
+    println(solarSystem[2])
+    println(solarSystem[3])
+    println(solarSystem[4])
+    println(solarSystem[5])
+    println(solarSystem[6])
+    println(solarSystem[7])
 }
