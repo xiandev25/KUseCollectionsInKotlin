afd2e83b0c76d6cb3e0373220ea4f54c09fbebe7
Xian Dev <xiandev25@gmail.com>
2025-11-25 20:55:27 +0100

    Define two arrays and add them together

--- src/Main.kt
+++ src/Main.kt
@@ -1,2 +1,5 @@
 fun main() {
+    val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
+    val gasPlanets = arrayOf("Jupiter", "Satan", "Uranus", "Neptune")
+    val solarSystem = rockPlanets + gasPlanets
 }
