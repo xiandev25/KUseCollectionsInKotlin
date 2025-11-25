94e8f74dc7b7965aa7e5c4743166e771ea1d12aa
Xian Dev <xiandev25@gmail.com>
2025-11-25 21:08:52 +0100

    Define a List object and access its size property

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,23 @@
 fun main() {
-    val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
-    val gasPlanets = arrayOf("Jupiter", "Satan", "Uranus", "Neptune")
+//    val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
+//    val gasPlanets = arrayOf("Jupiter", "Satan", "Uranus", "Neptune")
+//
+//    val solarSystem = rockPlanets + gasPlanets
+//
+//    println(solarSystem[0])
+//    println(solarSystem[1])
+//    println(solarSystem[2])
+//    println(solarSystem[3])
+//    println(solarSystem[4])
+//    println(solarSystem[5])
+//    println(solarSystem[6])
+//    println(solarSystem[7])
+//
+//    solarSystem[3] = "Little Earth"
+//    println(solarSystem[3])
+//
+//    solarSystem[8] = "Pluto"
 
-    val solarSystem = rockPlanets + gasPlanets
-
-    println(solarSystem[0])
-    println(solarSystem[1])
-    println(solarSystem[2])
-    println(solarSystem[3])
-    println(solarSystem[4])
-    println(solarSystem[5])
-    println(solarSystem[6])
-    println(solarSystem[7])
-
-    solarSystem[3] = "Little Earth"
-    println(solarSystem[3])
+    val solarSystem = listOf<String>("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    println(solarSystem.size)
 }
